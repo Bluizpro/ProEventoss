4b7fe1f8dfa9a113b44f32b595573684c06f7f79
bruno <bluizprogramador@gmail.com>
2022-08-15 16:49:28 -0300

    conectando com banco de dados

--- Back/src/ProEvento.API/Controllers/EventoController.cs
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEvento.API.DataEvento;
 using ProEvento.API.Models;
 
 namespace ProEvento.API.Controllers
@@ -12,41 +13,25 @@ namespace ProEvento.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,    
-            Tema = "Angula 11 e .Net 5",
-            Local = "Belo Horizonte",
-            Lote = "1° Lote",
-            QtdPessoas = 250,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-            ImagemURL = "foto.png"
-           },             
-            new Evento(){
-                EventoId = 2,
-            Tema = "Angula e sua novidades",
-            Local = "rio de janeiro",
-            Lote = "2° Lote",
-            QtdPessoas = 400,
-            DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
-            ImagemURL = "foto2.png"
-           }
-         }; 
-        public EventoController()
+
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento; 
+            return _context.Eventos ; 
       }
        [HttpGet ("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
 
         {
-            return _evento.Where(evento => evento.EventoId == id); 
+            return  _context.Eventos.FirstOrDefault(evento => evento.EventoId == id); 
       }
 
         [HttpPost]
